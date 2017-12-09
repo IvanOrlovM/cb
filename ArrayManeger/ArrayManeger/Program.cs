7ca9480ac8fb58e87289260e6a5e8a57667f695e
IvanOrlovM <orlovivan1907@outlook.com>
2017-12-10 00:11:12 +0200

    ArrayManeger is done

--- ArrayManeger/ArrayManeger/Program.cs
+++ ArrayManeger/ArrayManeger/Program.cs
@@ -1,48 +1,78 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Linq;
 
 namespace ArrayManeger
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            do
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("    -= Array maneger =-\n");
+
+                ArrayManager();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\n\nThe end!\nFor the exit press Q\nfor continue press any key.");
+            } while (Console.ReadKey().Key != ConsoleKey.Q);
+        }
+
+        /// <summary>
+        /// Array manager general logic.
+        /// </summary>
+        private static void ArrayManager()
+        {
+            var random = new Random();
+            int minValue = 0;
+            int maxValue = 100;
+
             int n = UserInput();
 
             int[] array = new int[n];
 
             for (int i = 0; i < array.Length; i++)
             {
-                array[i] = 100 + i;
+                var next = random.Next(minValue, maxValue);
+                array[i] = next;
             }
 
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("\nMax: ");
             Console.WriteLine(array.Max());
 
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("\nMin: ");
             Console.WriteLine(array.Min());
 
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("\nSum: ");
             Console.WriteLine(array.Sum());
 
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("\nAverage: ");
             Console.WriteLine(array.Average());
 
-            /*
-            string s;
-            if (i%2==0) 
-             s = "четное";
-            else 
-             s = "нечетное";
-            */
-
-            for (int index = 0; index < array.Length; index++)
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("\nArray: ");
+            foreach (var item in array)
             {
-                var value = array[index];
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\neven numbers: ");
+
+            foreach (var value in array)
+            {
                 if (value % 2 == 0)
                 {
-                    Console.WriteLine(value);  
+                    Console.Write(value + " ");
                 }
             }
-
-            Console.ReadKey();
         }
 
         /// <summary>
@@ -52,10 +82,10 @@ namespace ArrayManeger
         private static int UserInput()
         {
             int n;
-
+            
             do
             {
-                Console.Write("Введите число: ");
+                Console.Write("Enter the lenght array: ");
             } while (!int.TryParse(Console.ReadLine(), out n));
 
             return n;
