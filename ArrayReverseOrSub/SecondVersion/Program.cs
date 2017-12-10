23c79456bbeaf8177cb2af7ff72bc7438f69ea66
IvanOrlovM <orlovivan1907@outlook.com>
2017-12-10 13:56:15 +0200

    ArrayReverseOrSub add SecondVersion project

--- ArrayReverseOrSub/SecondVersion/Program.cs
+++ ArrayReverseOrSub/SecondVersion/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace SecondVersion
+﻿using System;
+
+namespace SecondVersion
 {
     class Program
     {
         static void Main()
         {
+            int [] arr = MyReverse(new[] {1, 3, 5});
+        }
+
+        private static int[] MyReverse(int[] array)
+        {
+            var reverse = new int[array.Length];
+
+            for (int i = array.Length - 1, j = 0; i >= 0 && j < reverse.Length; i--, j++)
+            {
+                reverse[j] = array[i];
+            }
+
+            return reverse;
         }
     }
 }
