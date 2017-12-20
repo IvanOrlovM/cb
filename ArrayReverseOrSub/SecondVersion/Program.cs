eb04c71b8a514b82824076b3289a733cfcbdf3f1
IvanOrlovM <orlovivan1907@outlook.com>
2017-12-20 21:21:10 +0200

    array reverse or sub

--- ArrayReverseOrSub/SecondVersion/Program.cs
+++ ArrayReverseOrSub/SecondVersion/Program.cs
@@ -6,14 +6,17 @@ namespace SecondVersion
     {
         static void Main()
         {
-            int [] arr = MyReverse(new[] {1, 3, 5});
+           int [] arr = MyReverse(new[] {1, 3, 5});
         }
 
         private static int[] MyReverse(int[] array)
         {
             var reverse = new int[array.Length];
 
-            for (int i = array.Length - 1, j = 0; i >= 0 && j < reverse.Length; i--, j++)
+            for (
+                int i = array.Length - 1, j = 0; // init managers variable
+                i >= 0 && j < reverse.Length; // block conditions
+                i--, j++) // change managers variable
             {
                 reverse[j] = array[i];
             }
