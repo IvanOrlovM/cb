28c4545cf92f6bc183cc459bed3c59c859d93fec
IvanOrlovM <orlovivan1907@outlook.com>
2018-01-07 00:00:55 +0200

    TrainManager init array trains.

--- Essential/007_Structures/TrainManager/TrainManager/Program.cs
+++ Essential/007_Structures/TrainManager/TrainManager/Program.cs
@@ -7,23 +7,41 @@ namespace TrainManager
     {
         static void Main(string[] args)
         {
-            Train train0 = UserInputTrain();
+            Train[] trains = new Train[args.Length];
+            for (int i = 0; i < args.Length; i++)
+            {
+                string param = args[i];
+                string[] trainParams = param.Split(',');
+                trains[i] = UserInputTrain(trainParams[0], trainParams[1], trainParams[2]);
+            }
 
             Console.ReadKey();
         }
 
-        private static Train UserInputTrain()
+        private static Train UserInputTrain(string trainNumber = "", string destination = "", string beginTime = "")
         {
-            Console.Write("Введите пункт назначения: ");
-            string destination = Console.ReadLine();
+            if (string.IsNullOrEmpty(destination))
+            {
+                Console.Write("Введите пункт назначения: ");
+                destination = Console.ReadLine();
+            }
 
-            Console.Write("Введите номер поезда: ");
-            string trainNumber = Console.ReadLine();
-            int number = int.Parse(trainNumber);
+            if (string.IsNullOrEmpty(trainNumber))
+            {
+                Console.Write("Введите номер поезда: ");
+                trainNumber = Console.ReadLine();
+            }
 
-            Console.Write("Введите время отправления (В формате HH:mm): ");
-            string beginTime = Console.ReadLine();
-            DateTime time = DateTime.ParseExact(beginTime, "HH:mm", CultureInfo.CurrentCulture);
+            var number = int.Parse(trainNumber);
+
+            if (string.IsNullOrEmpty(beginTime))
+            {
+                Console.Write("Введите время отправления (В формате HH:mm): ");
+                beginTime = Console.ReadLine();
+            }
+
+            var time = DateTime.ParseExact(beginTime, "HH:mm", CultureInfo.CurrentCulture);
+
             var train = new Train(destination, number, time);
             return train;
         }
