8ca8a0ee8a7ad68e185ad74097dbbee5b2c18107
IvanOrlovM <orlovivan1907@outlook.com>
2018-01-06 18:12:59 +0200

    DifferenceStructuresClasses is done

--- Essential/007_Structures/DifferenceStructuresClasses/DifferenceStructuresClasses/Program.cs
+++ Essential/007_Structures/DifferenceStructuresClasses/DifferenceStructuresClasses/Program.cs
@@ -1,26 +1,56 @@
-﻿namespace DifferenceStructuresClasses
+﻿using System;
+
+namespace DifferenceStructuresClasses
 {
     class Program
     {
-        static void ClassTaker(MyClass c)
+        private interface IChange
         {
-            c.change = "изменено";
+            string Change { get; set; }
         }
-        static void StructTaker(MyStruct s)
+
+        class MyClass: IChange
         {
-            s.change = "изменено";
+            public string Change { get; set; }
         }
-        class MyClass
+
+        struct MyStruct: IChange
         {
-            public string change; 
+            public string Change { get; set; }
         }
-        struct MyStruct
-        {
-            public string change;
-        }
+
         static void Main(string[] args)
         {
+            var myClass = new MyClass();
+            var myStruct = new MyStruct();
 
+            Console.WriteLine("-- перед вызовом методов --");
+            Show(myClass);
+            Show(myStruct);
+
+            ClassTaker(myClass);
+            StructTaker(myStruct);
+
+            Console.WriteLine("-- после вызова методов --");
+            Show(myClass);
+            Show(myStruct);
+        }
+
+        private static void Show(IChange obj)
+        {
+            Console.WriteLine($"{obj.GetType()}: {nameof(obj.Change)}: {obj.Change}");
+        }
+
+        static void ClassTaker(MyClass c)
+        {
+            Show(c);
+            c.Change = "изменено";
+        }
+
+        static void StructTaker(MyStruct s)
+        {
+            Show(s);
+            s.Change = "изменено";
         }
     }
 }
--- Essential/007_Structures/DifferenceStructuresClasses/DifferenceStructuresClasses/ReadMe.txt
+++ Essential/007_Structures/DifferenceStructuresClasses/DifferenceStructuresClasses/ReadMe.txt
@@ -1,9 +1,9 @@
 ﻿+ Создайте класс MyClass и структуру MyStruct, которые содержат в себе поля public string 
   change.
- -В классе Program создайте два метода:
-        - static void ClassTaker(MyClass myClass), который присваивает полю change экземпляра
++ В классе Program создайте два метода:
+        + static void ClassTaker(MyClass myClass), который присваивает полю change экземпляра
         myClass значение «изменено».
-        - static void StruktTaker(MyStruct myStruct), который присваивает полю change экземпляра
+        + static void StruktTaker(MyStruct myStruct), который присваивает полю change экземпляра
         myStruct значение «изменено».
 Продемонстрируйте разницу в использовании классов и структур:
     - создав в методе Main() экземпляры структуры и класса.
