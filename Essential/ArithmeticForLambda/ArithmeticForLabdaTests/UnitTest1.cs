00782f4ec7d5dc1f06ce6ceea81560eec4a698d5
IvanOrlovM <orlovivan1907@outlook.com>
2018-01-15 21:42:07 +0200

    ArithmeticForLambda created the unit tests project

---
+++ Essential/ArithmeticForLambda/ArithmeticForLabdaTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArithmeticForLabdaTests
+{
+    [TestClass]
+    public class CalcTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- Essential/ArithmeticForLambda/ArithmeticForLambda/Program.cs
+++ Essential/ArithmeticForLambda/ArithmeticForLambda/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace ArithmeticForLambda
+﻿using System;
+
+namespace ArithmeticForLambda
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
+
     }
 }
