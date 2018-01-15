6e8792bd629d573c01d8be32d2cec0f503a139b4
IvanOrlovM <orlovivan1907@outlook.com>
2018-01-15 23:58:31 +0200

    ArithmeticForLambda completed unit tests.

---
+++ Essential/ArithmeticForLambda/ArithmeticForLabdaTests/CalcTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using ArithmeticForLambda;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArithmeticForLabdaTests
+{
+    [TestClass]
+    public class CalcTest
+    {
+        private Calc _target;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _target = new Calc();
+        }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            double actual = _target.Add(2.5, 1.35);
+            Assert.AreEqual(expected: 3.85, actual: actual);
+        }
+
+        [TestMethod]
+        public void SubTest()
+        {
+            double actual = _target.Sub(2.5, 1.35);
+            Assert.AreEqual(expected: 1.15, actual: actual);
+        }
+
+       [TestMethod]
+        public void MulTest()
+        {
+            double actual = _target.Mul(2.5, 1.35);
+            Assert.AreEqual(expected: 3.375, actual: actual);
+        }
+
+        [TestMethod]
+        public void DivTest()
+        {
+            double actual = _target.Div(9, 2);
+            Assert.AreEqual(expected: 4.5, actual: actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DivExceptionTest()
+        {
+            double actual = _target.Div(9, 0);
+        }
+    }
+}
--- Essential/ArithmeticForLambda/ArithmeticForLambda/Calc.cs
+++ Essential/ArithmeticForLambda/ArithmeticForLambda/Calc.cs
@@ -1,4 +1,23 @@
-﻿namespace ArithmeticForLambda
+﻿using System;
+
+namespace ArithmeticForLambda
 {
-    public class Calc { }
+    public class Calc
+    {
+        public double Sub(double a, double b) => a - b;
+
+        public double Add(double a, double b) => a + b;
+
+        public double Mul(double a, double b) => a * b;
+
+        public Func<double, double, double> Div = (a, b) =>
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException(nameof(b));
+            }
+
+            return a / b;
+        };
+    }
 }
