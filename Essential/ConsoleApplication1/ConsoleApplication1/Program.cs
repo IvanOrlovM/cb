f01d04eb088b972a2d9a0a01dc6991d24967fb21
IvanOrlovM <orlovivan1907@outlook.com>
2018-01-20 23:10:03 +0200

    OverrideTest is done project

---
+++ Essential/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace OverrideTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Essential/OverrideTest/OverrideTest/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace OverrideTest
+{
+    class A
+    {
+        public A()
+        {
+            Console.WriteLine("ctor A.");
+        }
+
+        public virtual void Test()
+        {
+            Console.WriteLine("test from A.");
+        }
+    }
+
+    class B : A
+    {
+        public B()
+        {
+            Console.WriteLine("ctor B.");
+        }
+        public new void Test()
+        {
+            Console.WriteLine("test from B.");
+            base.Test();
+        }
+    }
+
+    class C : B
+    {
+        public C()
+        {
+            Console.WriteLine("ctor C.");
+        }
+        //public void Test()
+        //{
+        //    Console.WriteLine("test from C.");
+        //}
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+
+        static void Main1()
+        {
+            Console.Clear();
+            var a = new A(); // A
+            a.Test(); // A
+            Console.WriteLine("--");
+            var b = new B(); // A, B
+            b.Test(); // B, A
+
+            Console.WriteLine("--");
+            var c = new C(); // A, B, C
+            if (c is A)
+            {
+                var a1 = c as A;
+                a1.Test(); // A
+
+            }
+        }
+    }
+}
