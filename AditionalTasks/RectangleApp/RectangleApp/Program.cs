e0662c969b0434bb7625cb05e6098ae033324db0
IvanOrlovM <orlovivan1907@outlook.com>
2018-07-13 01:07:58 +0300

    first shanges rectangle app

--- AditionalTasks/RectangleApp/RectangleApp/Program.cs
+++ AditionalTasks/RectangleApp/RectangleApp/Program.cs
@@ -1,9 +1,46 @@
-﻿namespace RectangleApp
+﻿using System;
+
+namespace RectangleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // param
+            var length = 5;
+            var height = 4;
+
+            WriteLine(length);
+
+            // body
+            for (int i = 0; i < height - 2; i++)
+            {
+                Console.Write("*");
+                for (int j = 0; j < length - 2; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+                Console.Write("\n");
+            }
+
+            WriteLine(length);
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Write line
+        /// </summary>
+        /// <param name="length">lenght</param>
+        private static void WriteLine(int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("*");
+            }
+
+            Console.Write("\n");
         }
     }
 }
