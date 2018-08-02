1787c93d2fbaa92ab10665726dfd89959e45a2c2
IvanOrlovM <orlovivan1907@outlook.com>
2018-08-02 16:10:40 +0300

    AddressApplication is not ready

---
+++ AddressApplication/AddressApplication/AddressApplication/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Dynamic;
+
+namespace AddressApplication
+{
+    public class Address
+    {
+        public static int index
+        { get; set; }
+        public static string country
+        { get; set; }
+        public static string city
+        { get; set; }
+        public static string street
+        { get; set; }
+        public static int house
+        { get; set; }
+        public static int apartment
+        { get; set; }
+    }
+}
--- AddressApplication/AddressApplication/AddressApplication/Program.cs
+++ AddressApplication/AddressApplication/AddressApplication/Program.cs
@@ -1,12 +1,25 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
 
 namespace AddressApplication
 {
     class Program
     {
+        private static string Kiev;
+        private static string Mury;
+
         static void Main(string[] args)
         {
-
+            new Address();
+            {
+                Address.index = 08130;
+                Address.country = Kiev;
+                Address.city = Kiev;
+                Address.street = Mury;
+                Address.house = 11;
+                Address.apartment = 6;
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
