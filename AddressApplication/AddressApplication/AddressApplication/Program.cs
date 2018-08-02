381d32fe93a9d98cd065f356d3911f09135655e7
IvanOrlovM <orlovivan1907@outlook.com>
2018-08-02 11:29:10 +0300

    AddressApplication

---
+++ AddressApplication/AddressApplication/AddressApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AddressApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ AddressApplication/AddressApplication/AddressApplication/ReadMe.txt
@@ -0,0 +1,7 @@
+﻿namespace AddressApplication
+{
+    public class ReadMe
+    {
+        
+    }
+}
