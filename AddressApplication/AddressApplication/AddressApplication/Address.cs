637efa5af52ffb024bf1e6f996ee2013f6d9678b
mihail1orlov <mihail_orlov@hotmail.com>
2018-08-03 01:58:42 +0300

    Address app override ToString()

--- AddressApplication/AddressApplication/AddressApplication/Address.cs
+++ AddressApplication/AddressApplication/AddressApplication/Address.cs
@@ -11,5 +11,16 @@ namespace AddressApplication
         public string street { get; set; }
         public int house { get; set; }
         public int apartment { get; set; }
+
+        public override string ToString()
+        {
+            return
+                $"{nameof(index)}: {index}, " +
+                $"{nameof(country)}: {country}, " +
+                $"{nameof(city)}: {city}, " +
+                $"{nameof(street)}: {street}, " +
+                $"{nameof(house)}: {house}, " +
+                $"{nameof(apartment)}: {apartment}";
+        }
     }
 }
