1cabd07ed0a18a3a106e7a448822df6876bc4c03
IvanOrlovM <orlovivan1907@outlook.com>
2018-08-03 18:05:54 +0300

    Address init instans

---
+++ Essential/AddressApplication/AddressApplication/AddressApplication/Address.cs
@@ -0,0 +1,23 @@
+﻿namespace AddressApplication
+{
+    public class Address
+    {
+        public int Index { get; set; }
+        public string Country { get; set; }
+        public string city { get; set; }
+        public string street { get; set; }
+        public int house { get; set; }
+        public int apartment { get; set; }
+        public override string ToString()
+        {
+            return
+                $"{nameof(Index)}: {Index}, " +
+                $"{nameof(Country)}: {Country}, " +
+                $"{nameof(city)}: {city}, " +
+                $"{nameof(street)}: {street}, " +
+                $"{nameof(house)}: {house}, " +
+                $"{nameof(apartment)}: {apartment}";
+
+        }
+    }
+}
---
+++ Essential/AddressApplication/AddressApplication/AddressApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AddressApplication
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var address = new Address
+            {
+                Country = "111",
+                Index = 22,
+                apartment = 33,
+                city = "444",
+                house = 55,
+                street = "777"
+            };
+
+            Console.WriteLine(address);
+        }
+    }
+}
