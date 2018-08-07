8e0558127d6fd3e0893f35e193c35e87e3796753
IvanOrlovM <orlovivan1907@outlook.com>
2018-08-07 06:05:45 +0300

    L1T3LibraryApp is done

--- Essential/L1T3LibraryApp/L1T3LibraryApp/Program.cs
+++ Essential/L1T3LibraryApp/L1T3LibraryApp/Program.cs
@@ -7,13 +7,13 @@ namespace L1T3LibraryApp
         static void Main(string[] args)
         {
             var book = new Book();
-            Title title = new Title("title");
+            Title title = new Title("The Great Gatsby ");
             book.AddTitle(title);
 
-            Author author = new Author("author");
+            Author author = new Author("by F. Scott Fitzgerald ");
             book.AddAuthor(author);
 
-            Content content = new Content("content");
+            Content content = new Content("\n CONTENT... ");
             book.AddContent(content);
 
             book.Show();
