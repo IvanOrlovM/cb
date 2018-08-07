d736b74bb8a8daf3d49671e85ac4956b056bfe35
IvanOrlovM <orlovivan1907@outlook.com>
2018-08-07 06:34:22 +0300

    L1T4FiguresApp is underdone

--- Essential/L1T4FiguresApp/L1T4FiguresApp/Point.cs
+++ Essential/L1T4FiguresApp/L1T4FiguresApp/Point.cs
@@ -3,8 +3,23 @@
     public class Point
     {
         private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
         private int _y;
+        public int x
+        {
+            get { return _x; }
+        }
         private int _x;
+
+        public int y
+        {
+            get { return _y; }
+        }
 
         public Point(string name, int y, int x)
         {
