76bb4eb923bbd9b07a1c92ec6b1f8db3d278b6ba
IvanOrlovM <orlovivan1907@outlook.com>
2018-08-15 21:17:14 +0300

    L2T3Employee add user ctor

---
+++ Essential/L2T3EmployeeApp/L2T3EmployeeApp/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace L2T3EmployeeApp
+{
+    public class Employee
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public Employee(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+    }
+}
---
+++ Essential/L2T3EmployeeApp/L2T3EmployeeAppTest/EmployeeTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using L2T3EmployeeApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace L2T3EmployeeAppTest
+{
+    [TestClass]
+    public class EmployeeTest
+    {
+        private Employee _target;
+
+        [TestInitialize]
+        public void Init()
+        {
+            var firstName = "";
+            var lastName = "";
+            _target = new Employee(firstName, lastName);
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+
+        }
+    }
+}
