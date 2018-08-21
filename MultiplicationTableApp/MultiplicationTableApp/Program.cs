b73bc099a46fcb3bba9c373c0ee863700187c60a
IvanOrlovM <orlovivan1907@outlook.com>
2018-08-21 16:48:59 +0300

    MultiplicationTableApp is done

--- MultiplicationTableApp/MultiplicationTableApp/Program.cs
+++ MultiplicationTableApp/MultiplicationTableApp/Program.cs
@@ -6,9 +6,67 @@ namespace MultiplicationTableApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("X2");
+
             for (int i = 2, a = 2; i < 10; i++)
             {
                 Console.WriteLine($"{a}*{i}={a*i}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("X3");
+
+            for (int i = 2, a = 3; i < 10; i++)
+            {
+                Console.WriteLine($"{a}*{i}={a * i}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("X4");
+
+            for (int i = 2, a = 4; i < 10; i++)
+            {
+                Console.WriteLine($"{a}*{i}={a * i}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("X5");
+
+            for (int i = 2, a = 5; i < 10; i++)
+            {
+                Console.WriteLine($"{a}*{i}={a * i}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("X6");
+
+            for (int i = 2, a = 6; i < 10; i++)
+            {
+                Console.WriteLine($"{a}*{i}={a * i}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("X7");
+
+            for (int i = 2, a = 7; i < 10; i++)
+            {
+                Console.WriteLine($"{a}*{i}={a * i}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("X8");
+
+            for (int i = 2, a = 8; i < 10; i++)
+            {
+                Console.WriteLine($"{a}*{i}={a * i}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("X9");
+
+            for (int i = 2, a = 9; i < 10; i++)
+            {
+                Console.WriteLine($"{a}*{i}={a * i}");
             }
 
             Console.ReadLine();
