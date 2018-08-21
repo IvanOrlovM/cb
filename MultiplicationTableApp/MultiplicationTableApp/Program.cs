8ce1a92106bb9cda6f20f11f8192d7adbf5b8026
IvanOrlovM <orlovivan1907@outlook.com>
2018-08-21 08:34:33 +0300

    MultiplicationTableApp add table by 2

--- MultiplicationTableApp/MultiplicationTableApp/Program.cs
+++ MultiplicationTableApp/MultiplicationTableApp/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace MultiplicationTableApp
+﻿using System;
+
+namespace MultiplicationTableApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("2*2=4");
+            Console.WriteLine("2*3=6");
+            Console.WriteLine("2*4=8");
+            Console.WriteLine("2*5=10");
+            Console.WriteLine("2*6=12");
+            Console.WriteLine("2*7=14");
+            Console.WriteLine("2*8=16");
+            Console.WriteLine("2*9=18");
+            Console.ReadLine();
+
         }
     }
 }
