a40c4a48528a29a1a8ddfc12887184b229c73fa4
IvanOrlovM <orlovivan1907@outlook.com>
2018-08-22 19:38:25 +0300

    MultiplicationTableApp is ready for two till nine

--- MultiplicationTableApp/MultiplicationTableApp/Program.cs
+++ MultiplicationTableApp/MultiplicationTableApp/Program.cs
@@ -6,70 +6,19 @@ namespace MultiplicationTableApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("X2");
-
-            for (int i = 2, a = 2; i < 10; i++)
+            for (int i = 2; i < 10; i++)
             {
-                Console.WriteLine($"{a}*{i}={a*i}");
+                Console.WriteLine($"X{i}");
+                var str = "";
+                for (int j = 2; j < 10; j++)
+                {
+                    str = str + $"{i}*{j}={j * i}\n";
+                }
+
+                Console.WriteLine(str);
             }
 
-            Console.WriteLine();
-            Console.WriteLine("X3");
-
-            for (int i = 2, a = 3; i < 10; i++)
-            {
-                Console.WriteLine($"{a}*{i}={a * i}");
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("X4");
-
-            for (int i = 2, a = 4; i < 10; i++)
-            {
-                Console.WriteLine($"{a}*{i}={a * i}");
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("X5");
-
-            for (int i = 2, a = 5; i < 10; i++)
-            {
-                Console.WriteLine($"{a}*{i}={a * i}");
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("X6");
-
-            for (int i = 2, a = 6; i < 10; i++)
-            {
-                Console.WriteLine($"{a}*{i}={a * i}");
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("X7");
-
-            for (int i = 2, a = 7; i < 10; i++)
-            {
-                Console.WriteLine($"{a}*{i}={a * i}");
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("X8");
-
-            for (int i = 2, a = 8; i < 10; i++)
-            {
-                Console.WriteLine($"{a}*{i}={a * i}");
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("X9");
-
-            for (int i = 2, a = 9; i < 10; i++)
-            {
-                Console.WriteLine($"{a}*{i}={a * i}");
-            }
-
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
