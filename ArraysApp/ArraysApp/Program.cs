8af4ce47df5b443d96e5a47737085ce99cbdde6d
IvanOrlovM <orlovivan1907@outlook.com>
2018-08-22 22:35:03 +0300

    ArraysApp is printed

--- ArraysApp/ArraysApp/Program.cs
+++ ArraysApp/ArraysApp/Program.cs
@@ -6,6 +6,40 @@ namespace ArraysApp
     {
         static void Main(string[] args)
         {
+            MultiDimensional();
+            SingleDimensional();
+
+            Console.ReadKey();
+        }
+
+        private static void MultiDimensional()
+        {
+            int[][] array = new int[10][];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new int[10];
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    array[i][j] = int.Parse($"{i}{j}");
+                }
+            }
+            PrintArray(array);
+        }
+
+        public static void PrintArray(int[][] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    Console.Write(array[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void SingleDimensional()
+        {
             int[] array = new int[10];
             for (int i = 0; i < array.Length; i++)
             {
@@ -19,11 +53,8 @@ namespace ArraysApp
         {
             for (int i = 0; i < array.Length; i++)
             {
-                Console.Write(array[i]);
-                Console.Write(" ");
+                Console.Write(array[i] + " ");
             }
-
-            Console.ReadKey();
         }
     }
 }
