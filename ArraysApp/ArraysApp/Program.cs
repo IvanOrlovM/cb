1a01e1aa9e2149023f50b3271eddcd0c0d71db4f
IvanOrlovM <orlovivan1907@outlook.com>
2018-08-22 20:54:32 +0300

    ArraysApp add single-dimensional array

--- ArraysApp/ArraysApp/Program.cs
+++ ArraysApp/ArraysApp/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace ArraysApp
+﻿using System;
+
+namespace ArraysApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+
+            ShowArray(array);
+        }
+
+        public static void ShowArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                Console.Write(" ");
+            }
+
+            Console.ReadKey();
         }
     }
 }
