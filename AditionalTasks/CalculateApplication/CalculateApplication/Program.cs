606ca640bbcf1db4952f74b41f58616fcecc29f5
IvanOrlovM <orlovivan1907@outlook.com>
2018-11-05 18:48:12 +0200

    CalculateApplication is ready

--- AditionalTasks/CalculateApplication/CalculateApplication/Program.cs
+++ AditionalTasks/CalculateApplication/CalculateApplication/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace CalculateApplication
+﻿using System;
+
+namespace CalculateApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a: ");
+            string a = Console.ReadLine();
+            int num1 = int.Parse(a);
+            Console.Write("Enter b: ");
+            string b = Console.ReadLine();
+            int num2 = int.Parse(b);
+            Console.Clear();
+            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+            Console.ReadLine();
         }
     }
 }
