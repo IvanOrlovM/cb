0db7bcccc1b71726d22b1527fa44dacdc76d0479
IvanOrlovM <orlovivan1907@outlook.com>
2018-11-21 19:30:12 +0200

    CalculateApplication add table

--- AditionalTasks/CalculateApplication/CalculateApplication/Program.cs
+++ AditionalTasks/CalculateApplication/CalculateApplication/Program.cs
@@ -6,16 +6,72 @@ namespace CalculateApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("***************");
+            //for (int i = 0x2500; i <= 0x2570; i += 0x10)
+            //{
+            //    for (int c = 0; c <= 0xF; ++c)
+            //    {
+            //        Console.Write($"{i + c}{(char)(i + c)} ");
+            //    }
+
+            //    Console.WriteLine("/n");
+            //}
+
+            Console.WriteLine($"{(char)9484}" +
+                              $"{(char)9472}" +
+                              $"{(char)9472}" +
+                              $"{(char)9472}" +
+                              $"{(char)9472}" +
+                              $"{(char)9472}" +
+                              $"{(char)9472}" +
+                              $"{(char)9472}" +
+                              $"{(char)9472}" +
+                              $"{(char)9472}" +
+                              $"{(char)9472}" +
+                              $"{(char)9472}" +
+                              $"{(char)9472}" +
+                              $"{(char)9472}" +
+                              $"{(char)9488}");
             for (int i = 2; i < 10; i++)
             {
-                for (int j = 2; j < 10; j++)
+                for (int j = 2; j < 9; j++)
                 {
                     int a = j * i;
-                    Console.WriteLine($"* {i} * {j} = {a} {(a < 10 ? " " : "")} *");
+                    Console.WriteLine($"{(char)9474} {i} * {j} = {a} {(a < 10 ? " " : "")} {(char)9474}");
                 }
-                Console.WriteLine("***************");
+
+                Console.WriteLine($"{(char)9500}" +
+                                  $"{(char)9472}" +
+                                  $"{(char)9472}" +
+                                  $"{(char)9472}" +
+                                  $"{(char)9472}" +
+                                  $"{(char)9472}" +
+                                  $"{(char)9472}" +
+                                  $"{(char)9472}" +
+                                  $"{(char)9472}" +
+                                  $"{(char)9472}" +
+                                  $"{(char)9472}" +
+                                  $"{(char)9472}" +
+                                  $"{(char)9472}" +
+                                  $"{(char)9472}" +
+                                  $"{(char)9508}");
             }
+
+            Console.WriteLine($"{(char) 9492}" +
+                              $"{(char) 9472}" +
+                              $"{(char) 9472}" +
+                              $"{(char) 9472}" +
+                              $"{(char) 9472}" +
+                              $"{(char) 9472}" +
+                              $"{(char) 9472}" +
+                              $"{(char) 9472}" +
+                              $"{(char) 9472}" +
+                              $"{(char) 9472}" +
+                              $"{(char) 9472}" +
+                              $"{(char) 9472}" +
+                              $"{(char) 9472}" +
+                              $"{(char) 9472}" +
+                              $"{(char) 9496}");
+
             Console.ReadLine();
         }
     }
