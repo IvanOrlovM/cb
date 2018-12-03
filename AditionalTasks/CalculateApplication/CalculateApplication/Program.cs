908416142fddc303bccff23b1574b0794d5ba49a
IvanOrlovM <orlovivan1907@outlook.com>
2018-12-03 17:05:02 +0200

    ExpressionFileCalculator is empty

--- AditionalTasks/CalculateApplication/CalculateApplication/Program.cs
+++ AditionalTasks/CalculateApplication/CalculateApplication/Program.cs
@@ -16,6 +16,7 @@ namespace CalculateApplication
             //    Console.WriteLine("/n");
             //}
 
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"{(char)9484}" +
                               $"{(char)9472}" +
                               $"{(char)9472}" +
@@ -31,46 +32,76 @@ namespace CalculateApplication
                               $"{(char)9472}" +
                               $"{(char)9472}" +
                               $"{(char)9488}");
+
             for (int i = 2; i < 10; i++)
             {
-                for (int j = 2; j < 9; j++)
+                Console.ForegroundColor = ConsoleColor.White;
+                for (int j = 2; j < 10; j++)
                 {
                     int a = j * i;
-                    Console.WriteLine($"{(char)9474} {i} * {j} = {a} {(a < 10 ? " " : "")} {(char)9474}");
+
+                    // Console.WriteLine($"{(char)9474} {i} * {j} = {a} {(a < 10 ? " " : "")} {(char)9474}");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write($"{(char)9474}");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($" {i}");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(" * ");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($"{j}");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(" = ");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($"{a}  ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write($"{(a < 10 ? " " : "")}");
+                    Console.WriteLine($"{(char)9474}");
                 }
 
-                Console.WriteLine($"{(char)9500}" +
-                                  $"{(char)9472}" +
-                                  $"{(char)9472}" +
-                                  $"{(char)9472}" +
-                                  $"{(char)9472}" +
-                                  $"{(char)9472}" +
-                                  $"{(char)9472}" +
-                                  $"{(char)9472}" +
-                                  $"{(char)9472}" +
-                                  $"{(char)9472}" +
-                                  $"{(char)9472}" +
-                                  $"{(char)9472}" +
-                                  $"{(char)9472}" +
-                                  $"{(char)9472}" +
-                                  $"{(char)9508}");
-            }
+                Console.ForegroundColor = ConsoleColor.Green;
 
-            Console.WriteLine($"{(char) 9492}" +
-                              $"{(char) 9472}" +
-                              $"{(char) 9472}" +
-                              $"{(char) 9472}" +
-                              $"{(char) 9472}" +
-                              $"{(char) 9472}" +
-                              $"{(char) 9472}" +
-                              $"{(char) 9472}" +
-                              $"{(char) 9472}" +
-                              $"{(char) 9472}" +
-                              $"{(char) 9472}" +
-                              $"{(char) 9472}" +
-                              $"{(char) 9472}" +
-                              $"{(char) 9472}" +
-                              $"{(char) 9496}");
+                if (i < 9)
+                {
+                    Console.WriteLine($"{(char)9500}" +
+                                      $"{(char)9472}" +
+                                      $"{(char)9472}" +
+                                      $"{(char)9472}" +
+                                      $"{(char)9472}" +
+                                      $"{(char)9472}" +
+                                      $"{(char)9472}" +
+                                      $"{(char)9472}" +
+                                      $"{(char)9472}" +
+                                      $"{(char)9472}" +
+                                      $"{(char)9472}" +
+                                      $"{(char)9472}" +
+                                      $"{(char)9472}" +
+                                      $"{(char)9472}" +
+                                      $"{(char)9508}");
+
+
+                }
+                else
+                {
+                    Console.WriteLine(
+                        $"{(char)9492}" +
+                        $"{(char)9472}" +
+                        $"{(char)9472}" +
+                        $"{(char)9472}" +
+                        $"{(char)9472}" +
+                        $"{(char)9472}" +
+                        $"{(char)9472}" +
+                        $"{(char)9472}" +
+                        $"{(char)9472}" +
+                        $"{(char)9472}" +
+                        $"{(char)9472}" +
+                        $"{(char)9472}" +
+                        $"{(char)9472}" +
+                        $"{(char)9472}" +
+                        $"{(char)9496}");
+
+                }
+
+            }
 
             Console.ReadLine();
         }
