5850faa263790a29b8f654d452c5791fa7044fa2
IvanOrlovM <orlovivan1907@outlook.com>
2018-12-03 20:23:14 +0200

    ExpressionFileCalculator is working

--- AditionalTasks/ExpressionFileCalculator/ExpressionFileCalculator/Program.cs
+++ AditionalTasks/ExpressionFileCalculator/ExpressionFileCalculator/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
 
 namespace ExpressionFileCalculator
 {
+    // f F
     class Program
     {
         static void Main(string[] args)
         {
+            var lines = File.ReadAllLines("file.exp");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ AditionalTasks/ExpressionFileCalculator/ExpressionGenerator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ExpressionGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] contents = new string[10];
+            var random = new Random();
+            for (int i = 0; i < contents.Length; i++)
+            {
+                var a = random.Next(1, 10);
+                var b = random.Next(1, 10);
+                var indexOper = random.Next(1, 4);
+                var o = new[] {"+", "-", "*", "/"}[indexOper];
+                var exp = $"{a}{o}{b}=";
+                contents[i] = exp;
+            }
+            
+            File.AppendAllLines("File.exp", contents);
+        }
+    }
+}
